1e6f964515963b26ad4a5edf931bbcda9e13d8d3
Joostafer <joostafer@gmail.com>
2024-02-17 16:39:38 +0200

    First HW

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import image from './assets/header.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <img src={image} alt="My Image"/>
+        <h1>«Hello world»</h1>
+        <p>Hi, I`m a web programmer and this is my first application written in Reactiv</p>
+    </>
+  )
+}
+
+export default App
